4dde68d9550da8c2be693bc86089099eae8fd29c
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-19 17:18:18 -0400

    New small project.

---
+++ BMI/Main.java
@@ -0,0 +1,11 @@
+
+//Importing Scanner so we can read user input
+import java.util.Scanner;
+
+public class Main {
+    // Main method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the BMI calculator.");
+    }
+}
