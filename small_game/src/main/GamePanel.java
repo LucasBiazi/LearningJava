d94cd1b86699414822faeb6ba7ee09e965af5a6d
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-22 11:02:32 -0400

    Game screen.

--- small_game/src/main/GamePanel.java
+++ small_game/src/main/GamePanel.java
@@ -1,5 +1,25 @@
 package main;
 
-public class GamePanel {
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
 
+public class GamePanel extends JPanel {
+	// Screen settings
+	final int orignal_tile_size = 16; // 16x16
+	final int scale = 3; // 16 x 3 = 48px
+
+	final int tile_size = orignal_tile_size * scale; // 48px
+	// 4 by 3, game window (panel)
+	final int max_screen_column = 16;
+	final int max_screen_row = 12;
+	final int screen_width = max_screen_column * tile_size; // 38 x 16 = 758px
+	final int screen_height = max_screen_row * tile_size; // 48 x 12 = 576px
+
+	public GamePanel() {
+		// Sets the size of the window
+		this.setPreferredSize(new Dimension(screen_width, screen_height));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true); // Makes the rendering more performatic
+	}
 }
--- small_game/src/main/Main.java
+++ small_game/src/main/Main.java
@@ -6,12 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// Creating a new window
+		// Creating a new frame
 		JFrame window = new JFrame();
 		// Close window so it won't run in the background
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false); // Not resizable
 		window.setTitle("Small Game"); // Window title
+		
+		GamePanel game_panel = new GamePanel();
+		window.add(game_panel);
+		window.pack();
 		
 		window.setLocationRelativeTo(null); // Spawn window on the center of the screen
 		window.setVisible(true); // Make it visible
